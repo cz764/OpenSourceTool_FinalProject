714ac43bc89e6744eaa75c0cf9d3754145ee2733
Chen Zhu <cz764@nyu.edu>
2014-12-05 15:23:40 -0500

    front end works

---
+++ jane.py
@@ -0,0 +1,64 @@
+# [START imports]
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+# [START main_page]
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        greetings = "rendered by Jane with bootstrap"
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+        template_values = {
+            'jane': greetings,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+        template = JINJA_ENVIRONMENT.get_template('layout.html')
+        self.response.write(template.render(template_values))
+    
+# [END main_page]
+
+# [START BootStrp_page]
+class AskQuestion(webapp2.RequestHandler):
+    def get(self):
+        greetings = "rendered by Jane without BootStrap"
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'jane': greetings,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('ask.html',parent='layout.html')
+        self.response.write(template.render(template_values))    
+# [END BootStrap_page]
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/index', MainPage),
+    ('/ask', AskQuestion),
+], debug=True)
