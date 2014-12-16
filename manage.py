d7e512adcde6377318e75859c0e083a8f9cd5e64
Chen Zhu <cz764@nyu.edu>
2014-12-15 20:37:18 -0500

    image works

--- manage.py
+++ manage.py
@@ -27,6 +27,8 @@ class ManagePage(webapp2.RequestHandler):
 	def get(self):
 		if users.get_current_user():
 			template_values = getQuestionList(self, True)
+			template_values["isManaging"] = True
+			template_values["user"] = users.get_current_user()
 			writingToTemplate(self, template_values)
 		else:
 			template_values = getLoginTemplateStatus(self, users)
@@ -36,49 +38,57 @@ class ManagePage(webapp2.RequestHandler):
 
 class ImageHandler(webapp2.RequestHandler):
     def get(self, url):
-	# picture = Image.query(Image.url == url).get()
-	# if picture.ifile:
-	# 	self.response.headers['Content-Type'] = 'image/png'
-	# 	self.response.out.write(picture.ifile)
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write("in ImageHandler")
+		picture = Image.query(Image.url == url).get()
+		if picture.ifile:
+			self.response.headers['Content-Type'] = 'image/png'
+			self.response.out.write(picture.ifile)
+		# self.response.headers['Content-Type'] = 'text/plain'
+		# self.response.write("in ImageHandler")
 
 class Uploader(webapp2.RequestHandler):
-    def get(self):
-		# header(self)
-		# display = JINJA_ENVIRONMENT.get_template('temp_upload.html')
-		# self.response.write(display.render())
-		# footer(self)
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write("in uploader")
-    def post(self):
-		# header(self)
-		# if users.get_current_user():
-		# 	res = self.request.get('img')
-		# 	image = Image()
-		# 	image.ifile = res
-		# 	image.user = str(users.get_current_user())
-		# 	image_key = image.put()
-		# 	image = Image.get_by_id(image_key.id())
-		# 	image.url = str(image_key.id())+"_"+self.request.params['img'].filename
-		# 	image.put()
-		# self.response.write('<p class="main">Upload Success!</p>')
-		# footer(self)
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write("in uploader post method")
+	def get(self):
+		template_values = {}
+		template_values["display_uploader"] = True
+		user = users.get_current_user()
+		images = Image.get_author(user)
+		template_values["images"] = images
+		template = JINJA_ENVIRONMENT.get_template('upload.html')
+		self.response.write(template.render(template_values))	
+	def post(self):
+		status = {}
+		if users.get_current_user():
+			img = self.request.get('img')
+			image = Image()
+			image.ifile = img
+			image.user = users.get_current_user()
+			image_key = image.put()
+			image = Image.get_by_id(image_key.id())
+			image.url = str(image_key.id())+"_"+self.request.params['img'].filename
+			image.put()
+			status["status"] = "Success!"
+		else:
+			status["status"] = "Failed:("
+		status["display_uploader"] = False
+
+		user = users.get_current_user()
+		images = Image.get_author(user)
+		status["images"] = images
+		template = JINJA_ENVIRONMENT.get_template('upload.html')
+		self.response.write(template.render(status))
+	
 
 class DeleteImage(webapp2.RequestHandler):
     def get(self, key):
-	# if users.get_current_user():
-	# 	img = Image.get_by_id(int(key))
-	# 	user = str(users.get_current_user())
-	# 	if img.user == user:
-	# 		img.key.delete()
-	# 	self.response.write('<p class="main"><b>Success!</b></p>')
-	# else:
-	# 	self.response.write('''<p class="main">You don't have permission!</p>''')
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write("in DeleteImage")
+		if users.get_current_user():
+			img = Image.get_by_id(int(key))
+			user = users.get_current_user()
+			if img.user == user:
+				img.key.delete()
+			self.response.write('<p class="main"><b>Success!</b></p>')
+		else:
+			self.response.write('''<p class="main">You don't have permission!</p>''')
+		# self.response.headers['Content-Type'] = 'text/plain'
+		# self.response.write("in DeleteImage")
 	
 
 
